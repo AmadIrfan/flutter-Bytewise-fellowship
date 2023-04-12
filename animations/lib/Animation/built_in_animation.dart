0bd68b69ed9c2b90067cd5d3bf57013270874958
Amad Irfan <amadirfan443@gmail.com>
2023-04-12 20:42:32 +0500

    animtions

---
+++ animations/lib/Animation/built_in_animation.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class BuiltInAnimation extends StatefulWidget {
+  const BuiltInAnimation({Key? key}) : super(key: key);
+
+  @override
+  State<BuiltInAnimation> createState() => _BuiltInAnimationState();
+}
+
+class _BuiltInAnimationState extends State<BuiltInAnimation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ animations/lib/Animation/user_define_animation.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class UserDefineAnimation extends StatefulWidget {
+  const UserDefineAnimation({Key? key}) : super(key: key);
+
+  @override
+  State<UserDefineAnimation> createState() => _UserDefineAnimationState();
+}
+
+class _UserDefineAnimationState extends State<UserDefineAnimation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
